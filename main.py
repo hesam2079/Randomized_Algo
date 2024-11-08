2d06f0e130fbf75e9a6e6599194a0e6880fbe8c5
hesam2079 <hesam2079@gmail.com>
2024-11-08 23:16:20 +0330

    this is the last one i've write and send to TA version 1.2

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from xmlrpc.client import boolean
 
 from class_node import Node
-from random import randint
+from random import randint, shuffle
 
 
 def generate_nodes():
@@ -19,13 +19,14 @@ def generate_messages(): # all nodes generate the message
     return messages
 
 def generate_delivery_list(): # create random delivery list
-    delivery_list = []
+    global drop_messages_ratio
+    number_of_drops = drop_messages_ratio * number_of_nodes
+    delivery_list = [0] * number_of_drops + [1] * (number_of_nodes - number_of_drops)
+    shuffle(delivery_list)
     global drop_message_flag
-    for i in range(len(nodes)):
-        random_value = randint(0, 1)
-        if random_value == 0:
-            drop_message_flag = True
-        delivery_list.append(random_value)  # generate random delivery list
+    for i in range(len(delivery_list)):
+        if delivery_list[i] == 0:
+            drop_message_flag = True  # generate random delivery list
     return delivery_list
 
 def deliver_messages(messages, delivery_list):
@@ -63,26 +64,26 @@ def create_table():
     print("└───────┴──────────────────────┴──────────────────────┴────────────┴──────────┘")
 
 def check_validity():
+    global false_validity_counter
     if (((all(not v for v in value) and all(not d for d in decisions)) or
             (all(value) and not drop_message_flag and all(decisions))) or
             (any(value) and not all(value))):
         return True
     else:
-        global validity_counter
-        validity_counter += 1
+        false_validity_counter += 1
         return False
 
 def check_agreement():
+    global false_agreement_counter
     if all(decisions) or not any(decisions):
         return True
     else:
-        global agreement_counter
-        agreement_counter += 1
+        false_agreement_counter += 1
         return False
 
 def calculate_validity_agreement_per_repeats():
-    validity_percentage = validity_counter / number_of_simulations
-    agreement_percentage = agreement_counter / number_of_simulations
+    validity_percentage = false_validity_counter * 1.0 / number_of_simulations
+    agreement_percentage = false_agreement_counter * 1.0/ number_of_simulations
     return agreement_percentage, validity_percentage
 
 
@@ -90,7 +91,10 @@ if __name__ == "__main__":
     r = int(input("Enter number of rounds : "))
     number_of_nodes = int(input("Enter number of nodes : "))
     number_of_simulations = int(input("Enter number of simulations : "))
+    drop_messages_ratio = int(input("Enter drop messages ratio (your number / 100) : "))
     output_data = []
+    false_validity_counter = 0
+    false_agreement_counter = 0
     for i in range(number_of_simulations):
         value = [] # value of all nodes
         nodes = generate_nodes() # generate all nodes and append all nodes initial value into the value[]
@@ -98,16 +102,14 @@ if __name__ == "__main__":
         message_passing_simulation() # simulate massages pass through nodes
         decisions = [] # decisions made by nodes after massages passed
         result() # append all nodes decisions into decisions[]
-        validity_counter = 0
-        agreement_counter = 0
-        validity = check_validity()
-        agreement = check_agreement()
-        agreement_percentage, validity_percentage = calculate_validity_agreement_per_repeats()
-        print(agreement_counter, validity_percentage)
+        validity = check_validity() # validity checker
+        agreement = check_agreement() # agreement checker
+        agreement_percentage, validity_percentage = calculate_validity_agreement_per_repeats() # calculate validity and agreement
 
-
+        # create sample output and add this loop of simulation at the end of simulation's datas
         sample_output = (i, value, decisions, agreement, validity)
         output_data.append(sample_output)
-    last_line_sample = ("-", "-", "-", agreement_counter, validity_counter)
+    # the last line of data should be percentages
+    last_line_sample = ("-", "-", "-", agreement_percentage, validity_percentage)
     output_data.append(last_line_sample)
     create_table()
